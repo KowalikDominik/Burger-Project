1cc55066d236beb6cadc156e0c217b3bb8cf5676
dominik <domi.kowalik@gmail.com>
2021-03-17 14:09:08 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 
 const app = (
-		<BrowserRouter basename="/Burger-Project">
+		<BrowserRouter basename="/Burger-Project/build/">
 			<App />
 		</BrowserRouter>
 		);
